2dd3f2d8b84d5f5feefc534279c3b626d2f4622f
ibrahimKanber <ikanber45@gmail.com>
2020-09-05 12:25:28 +0200

    completed version 1

--- script.js
+++ script.js
@@ -4,31 +4,57 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movieselect");
 
+populateUI();
 
 ///Save selected movie index and price
+
+
+///Total Price and Ticket counter
+let ticketPrice = +movieSelect.value;
+
 let setMovieData = (movieIndex, optionValue) => {
     localStorage.setItem("selectedMovieIndex", movieIndex);
     localStorage.setItem("selectedMoviePrice", optionValue)
 
 }
 
-///Total Price and Ticket counter
-let ticketPrice = +movieSelect.value;
-let updateSelectedCount = () => {
+////update total
+
+function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll(".row .seat.selected");
     const indexOfSeats = [...selectedSeats].map(seat => [...seatAll].indexOf(seat))
-    console.log(indexOfSeats)
     localStorage.setItem("selectedSeats", JSON.stringify(indexOfSeats))
 
     const counter = selectedSeats.length;
     count.innerHTML = counter;
     total.innerHTML = ticketPrice * counter
 };
+////Get data from localStorage andd populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seatAll.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add("selected")
+            }
+        })
+    }
+
+    const selectedMovieIndex = localStorage.getItem("selectedMovieIndex")
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+        console.log(movieSelect.selectedIndex)
+    }
+
+
+
+}
+
 ////movie select
 movieSelect.addEventListener("change", e => {
     ticketPrice = +e.target.value;
     setMovieData(e.target.selectedIndex, e.target.value);
-    console.log(typeof e.target.selectedIndex, typeof e.target.value);
+    updateSelectedCount();
 })
 
 
@@ -41,3 +67,5 @@ seatAll.forEach((seat) => seat.addEventListener("click", (e) => {
         updateSelectedCount();
     }
 }))
+
+updateSelectedCount()
