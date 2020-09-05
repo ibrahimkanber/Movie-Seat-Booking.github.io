938c588749a1d34215a0e888073982749cac5d33
ibrahimKanber <ikanber45@gmail.com>
2020-09-05 10:43:22 +0200

    Save Data To Local Storage

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 const container = document.querySelector(".containers")
-const seatAll = document.querySelectorAll(".seat");
+const seatAll = document.querySelectorAll(".row .seat");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movieselect");
 
-let price = +movieSelect.value
 
-console.log(price)
+///Save selected movie index and price
+let setMovieData = (movieIndex, optionValue) => {
+    localStorage.setItem("selectedMovieIndex", movieIndex);
+    localStorage.setItem("selectedMoviePrice", optionValue)
 
-let supdateSelectedCount = () => {
-    const selectedSeats = document.querySelectorAll("movieselect")
-    console.log(selectedSeats)
 }
 
-// seatAll.forEach((seat) => seat.addEventListener("click", (e) => {
-//     const classlist = e.target.classList
-//     if (classlist.contains("seat") &&
-//         !classlist.contains("occupied")) {
-//         classlist.toggle("selected")
-//         console.log(classlist)
-//         updateSelectedCount();
-//     }
-// }))
+///Total Price and Ticket counter
+let ticketPrice = +movieSelect.value;
+let updateSelectedCount = () => {
+    const selectedSeats = document.querySelectorAll(".row .seat.selected");
+    const indexOfSeats = [...selectedSeats].map(seat => [...seatAll].indexOf(seat))
+    console.log(indexOfSeats)
+    localStorage.setItem("selectedSeats", JSON.stringify(indexOfSeats))
+
+    const counter = selectedSeats.length;
+    count.innerHTML = counter;
+    total.innerHTML = ticketPrice * counter
+};
+////movie select
+movieSelect.addEventListener("change", e => {
+    ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
+    console.log(typeof e.target.selectedIndex, typeof e.target.value);
+})
+
+
+///Seat click event
+seatAll.forEach((seat) => seat.addEventListener("click", (e) => {
+    const classlist = e.target.classList;
+    if (classlist.contains("seat") &&
+        !classlist.contains("occupied")) {
+        classlist.toggle("selected")
+        updateSelectedCount();
+    }
+}))
